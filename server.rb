526c7c2e7a6edaa931097f4c37e3b88b19df3d9b
Your Name <you@example.com>
2018-12-13 12:11:05 -0500

    added database_url to active record and ended def

--- server.rb
+++ server.rb
@@ -7,6 +7,7 @@ if ENV['RACK_ENV'] == 'development'
 set :database, {adapter: 'sqlite3', database: 'database.sqlite3'}
 else
   ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+end
 
 class User < ActiveRecord::Base
 end
