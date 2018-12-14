4978e6a373164a37c23d3b6fc8556b239233b402
Your Name <you@example.com>
2018-12-14 09:48:36 -0500

    recoded files

--- server.rb
+++ server.rb
@@ -4,9 +4,9 @@ require 'sqlite3'
 enable :sessions
 
 if ENV['RACK_ENV'] == 'development'
-  set :database, {adapter: 'sqlite3', database: 'database.sqlite3'}
-else
   ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+else
+  set :database, {adapter: 'sqlite3', database: 'database.sqlite3'}
 end
 
 class User < ActiveRecord::Base
@@ -15,39 +15,12 @@ end
 class Post < ActiveRecord::Base
 end
 
-
 get '/' do
-  erb :'/users/home'
-
-end
-
-get '/posts/new' do
-  if session['user_id'] == nil
-    puts "Need to sign in"
-    redirect '/'
-  end
-    erb :'/posts/new'
-end
-
-
-
-
-
-post '/posts/new' do
-  puts "Posted"
-  @post = Post.new(title: params['title'], username: params['username'], content: params['content'])
-  @post.save
-   p @post
-  redirect "/posts/#{@post.id}"
-end
-
-get '/posts/:id' do
-  @post = Post.find(params['id'])
-  erb :'/posts/show'
+  erb :home
 end
 
 get '/login' do
-   erb :'/users/login'
+  erb :'/users/login'
 end
 
 post '/login' do
@@ -55,20 +28,17 @@ post '/login' do
   if user != nil
     if user.password == params['password']
       session[:user_id] = user.id
-      # redirect "/users/#{user.id}"
-      redirect "/posts/new"
-      end
+      redirect "/users/#{user.id}"
     end
   end
-
-
-get '/users/new' do
-  erb :'/users/new'
 end
 
-get '/users/:id' do
-  @user = User.find(params['id'])
-  erb :'users/show'
+get "/users/new" do
+  if session['user.id'] != nil
+    p "Signed In"
+    redirect "/"
+  end
+  erb :"/users/new"
 end
 
 post '/users/new' do
@@ -77,3 +47,127 @@ post '/users/new' do
   session[:user_id] = @user.id
   redirect "/users/#{@user.id}"
 end
+
+  get "/users/:id" do
+    @user = User.find(params['id'])
+    @posts = Post.where(user_id: params['id'])
+    erb :'/users/show'
+  end
+
+  get '/users/?' do
+    @users = User.all
+    erb :'/users/members'
+  end
+
+  post '/users/:id' do
+    @user = User.find(params['id'])
+    @user.destroy
+    redirect '/'
+  end
+
+  get '/posts/new' do
+    if session[:user_id] == nil
+      puts "You Need To Be Logged In"
+      redirect '/'
+    end
+    erb :'/posts/new'
+  end
+
+  get '/posts/?' do
+    @posts =Post.all.reverse
+    erb :'posts/timeline'
+  end
+
+  post '/posts/new' do
+    puts "Posted"
+    @post = Post.new(title: params['title'], content: params['content'], username: params['username'])
+    @post.save
+    redirect :"users/#{session["user_id"]}"
+  end
+
+  get '/posts/:id' do
+    @post = Post.find_by(params['username'])
+    @post.destroy
+    redirect :"users/#{session["user_id"]}"
+
+  end
+
+  post "/logout" do
+  session['user_id'] = nil
+  redirect "/"
+end
+
+# get '/' do
+#   session[:user_id] == nil
+#   erb :'/users/home'
+#
+# end
+#
+# post '/login' do
+#   user = User.find_by(username: params['username'])
+#   if user != nil
+#     if user.password == params['password']
+#       session[:user_id] = user.id
+#       # redirect "/users/#{user.id}"
+#       redirect "/posts/new"
+#     else
+#       redirect "/users/new"
+#     end
+#   end
+# end
+#
+# get '/users/new' do
+#   erb :'/users/new'
+# end
+#
+#
+#
+#   post '/users/new' do
+#     @user = User.new(name: params['name'], username: params['username'], birthday: params['birthday'], email: params['email'], password: params['password'])
+#     @user.save
+#     session[:user_id] = @user.id
+#     redirect "/users/#{@user.id}"
+#   end
+#
+# get '/users/:id' do
+#   @user = User.find(params['id'])
+#   @posts = Post.all
+#   erb :'users/show'
+# end
+#
+# get '/users/?' do
+#   @users = User.all
+#   erb :'/users/home'
+# end
+#
+# post '/posts/new' do
+#   puts "Posted"
+#   @post = Post.new(title: params[:title], username: params[:username], content: params[:content], user_id: session[:user_id])
+#   @post.save
+#   redirect "/posts/#{@post.id}"
+# end
+#
+# get '/posts/new' do
+#   if session[:user_id] == nil
+#     puts "Need to sign in"
+#     redirect '/'
+#   end
+#   erb :'/posts/new'
+# end
+#
+#
+# get '/posts/:id' do
+#   @post = Post.find(params['id'])
+#   erb :"/posts/show"
+# end
+#
+# get '/posts/?' do
+#   @posts = Post.all
+#   erb :'posts/timeline'
+# end
+#
+#
+#
+# get '/login' do
+#   erb :'/users/login'
+# end
